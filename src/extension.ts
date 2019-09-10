b6126368f5a367188105d1f165cd970b23664ad8
creaink <455372644@qq.com>
2019-09-10 09:31:47 +0800

    Copy markdown title as refer link

--- src/extension.ts
+++ src/extension.ts
@@ -10,17 +10,40 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "markdown-tools" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    function copyReferLink(uri?: vscode.Uri) {
+		var isFailed = true;
+		if (uri instanceof vscode.Uri) {
+			var relPath = vscode.workspace.asRelativePath(uri.path);
+			const activeEditor = vscode.window.activeTextEditor;
+			if (activeEditor) {
+				const curLinePosition = activeEditor.selection.active;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+				let curLineSelection = new vscode.Selection(curLinePosition.with(curLinePosition.line, 0),
+															curLinePosition.with(curLinePosition.line+1, 0));
+				let curLineText = activeEditor.document.getText(curLineSelection).replace(/[\n\r]/g,'');
+				if (curLineText.includes("#")) {
+					let herfName = curLineText.replace(/^#+\s/, '');
+					// delete dummy '#', replace no-first ' ' with '-'
+					let herfLinkShort = curLineText.replace(/(#+)(\1)\s/, '$2').replace(/\s/g, '-');
+					let herfLinkLong = '/' + relPath + herfLinkShort;
+					let herf = `[${herfName}](${herfLinkLong})`;
+					vscode.env.clipboard.writeText(herf);
+					isFailed = false;
+				}
+			}
+		}
+		if (isFailed) {
+			vscode.window.showInformationMessage('Error occurred when copy markdown refer link!');
+		}
+	}
 
-	context.subscriptions.push(disposable);
+	// 注册 command
+	context.subscriptions.push(
+        vscode.commands.registerCommand(
+            "markdown-tools.copyReferLink",
+            copyReferLink,
+        ),
+	);
 }
 
 // this method is called when your extension is deactivated
