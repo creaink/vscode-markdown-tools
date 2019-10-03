f22892410e8c04183acbfe85628904568c8fd4b1
creaink <455372644@qq.com>
2019-10-03 18:30:52 +0800

    add copy file link from file browser

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Useful tools of VSCode plugin for markdown.
 
 ## Features
 
-- Copy markdown title as refer link
+- Copy as markdown refer link
+  - copy markdown current line title or file as refer link
+  - copy file refer link from file browser
 - [ ] Convert `[link][url]` to `[link][link-id]` with `[link-id]: url`
 
 ## Requirements
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { worker } from 'cluster';
+
+const imgFileType = [
+	"png",
+	"jpg",
+	"jpeg",
+	"bmp",
+	"gif"
+]
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,39 +19,84 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "markdown-tools" is now active!');
 
-    function copyReferLink(uri?: vscode.Uri) {
-		var isFailed = true;
-		if (uri instanceof vscode.Uri) {
-			var relPath = vscode.workspace.asRelativePath(uri.path);
-			const activeEditor = vscode.window.activeTextEditor;
-			if (activeEditor) {
-				const curLinePosition = activeEditor.selection.active;
+	/**
+	 * 判断 uri 是否是当前正编辑的文件
+	 * @param uri context uri
+	 */
+	function uriIsActiveEditor(uri: vscode.Uri) {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (activeEditor) return activeEditor.document.fileName == uri.fsPath;
+		else return false;
+	}
 
-				let curLineSelection = new vscode.Selection(curLinePosition.with(curLinePosition.line, 0),
-															curLinePosition.with(curLinePosition.line+1, 0));
-				let curLineText = activeEditor.document.getText(curLineSelection).replace(/[\n\r]/g,'');
-				if (curLineText.includes("#")) {
-					let herfName = curLineText.replace(/^#+\s/, '');
-					// delete dummy '#', replace no-first ' ' with '-'
-					let herfLinkShort = curLineText.replace(/(#+)(\1)\s/, '$2').replace(/\s/g, '-');
-					let herfLinkLong = '/' + relPath + herfLinkShort;
-					let herf = `[${herfName}](${herfLinkLong})`;
-					vscode.env.clipboard.writeText(herf);
-					isFailed = false;
+
+	function getReferLinkFromRelWorkspacePath(relWorkspacePath:string) {
+		// 从路径当中获取文件名
+		let fileName = relWorkspacePath.replace(/^.*[\\\/]/, '');
+		return `[${fileName}](/${relWorkspacePath})`
+	}
+
+	/**
+	 * 获取当前文件的参考链接
+	 * 如果是 md 文件是激活当前文件，1、当前行是合法标题，则返回标题作为链接，2、否则返回整个文件作为链接
+	 * 如果是其他类型文件，从文件管理或者当前激活，返回文件链接
+	 */
+	function getActiveFileReferLink() {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) return null;
+
+		let relWorkspacePath = vscode.workspace.asRelativePath(activeEditor.document.fileName);
+		const langId = activeEditor.document.languageId.toLowerCase();
+
+		if (langId == 'markdown') {
+			const curLinePosition = activeEditor.selection.active;
+
+			let curLineSelection = new vscode.Selection(curLinePosition.with(curLinePosition.line, 0),
+														curLinePosition.with(curLinePosition.line+1, 0));
+			let curLineText = activeEditor.document.getText(curLineSelection).replace(/[\n\r]/g,'');
+			if (curLineText.startsWith("#")) {
+				// markdown 标题的链接
+				let herfName = curLineText.replace(/^#+\s/, '');
+				// delete dummy '#', replace no-first ' ' with '-'
+				let herfLinkShort = curLineText.replace(/(#+)(\1)\s/, '$2').replace(/\s/g, '-');
+				let herfLinkLong = '/' + relWorkspacePath + herfLinkShort;
+				let herf = `[${herfName}](${herfLinkLong})`;
+				return herf;
+			} else {
+				return getReferLinkFromRelWorkspacePath(relWorkspacePath);
+			}
+		} else {
+			return getReferLinkFromRelWorkspacePath(relWorkspacePath);
+		}
+	}
+
+    function copyReferLink(uri?: vscode.Uri) {
+		var referLink = null;
+		if (uri instanceof vscode.Uri) {
+			if (uriIsActiveEditor(uri)) {
+				// 当前操作的为正在编辑的文件
+				referLink = getActiveFileReferLink();
+			} else {
+				// 非当前编辑文件，即文件浏览器
+				let relWorkspacePath = vscode.workspace.asRelativePath(uri.path);
+				let fileType = relWorkspacePath.split('.').pop();
+				if (fileType && imgFileType.includes(fileType)) {
+					// 如果是图片加上 ! 的格式，图片查看界面不能右键菜单
+					referLink = '!' + getReferLinkFromRelWorkspacePath(relWorkspacePath);
+				} else {
+					referLink = getReferLinkFromRelWorkspacePath(relWorkspacePath);
 				}
 			}
+			if (referLink != null) vscode.env.clipboard.writeText(referLink);
 		}
-		if (isFailed) {
+		if (referLink == null) {
 			vscode.window.showInformationMessage('Error occurred when copy markdown refer link!');
 		}
 	}
 
 	// 注册 command
 	context.subscriptions.push(
-        vscode.commands.registerCommand(
-            "markdown-tools.copyReferLink",
-            copyReferLink,
-        ),
+        vscode.commands.registerCommand("markdown-tools.copyReferLink", copyReferLink),
 	);
 }
 
